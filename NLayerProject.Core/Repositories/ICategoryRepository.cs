e842a3e74c5672e6834993b4e443a9b9651a8585
Batuhan HASBAL <batuhsbl28@gmail.com>
2021-09-06 19:35:19 +0300

    Add project files.

---
+++ NLayerProject.Core/Repositories/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using NLayerProject.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerProject.Core.Repositories
+{
+    public interface ICategoryRepository :IRepository<Category> //Irepository den miras aldık
+    {
+        Task<Category> GetWithProductByIdAsync(int categoryId);
+    }
+}
---
+++ NLayerProject.Core/Services/IService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerProject.Core.Services
+{
+    public interface IService<TEntity> where TEntity:class
+    {
+        Task<TEntity> GetByIdAsync(int id); //id ye göre ürün getir.
+        Task<IEnumerable<TEntity>> GetAllAsync(); //tüm nesneleri getir
+        Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity, bool>> predicate);    //find(x=>x.id =23)
+        // category.SingleOrDefaultAsync(x=>x.name="kalem")
+        Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
+        Task AddAsync(TEntity entity);
+        Task AddRangeAsync(IEnumerable<TEntity> entities);
+        void Remove(TEntity entity);
+        void RemoveRange(IEnumerable<TEntity> entity);
+        TEntity Update(TEntity entity);
+    }
+}
